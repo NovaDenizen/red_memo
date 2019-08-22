28f1c73b21206b3739ad3acb948532dc877eb122
Ken Bateman <novadenizen@gmail.com>
2019-08-22 15:41:04 -0400

    MemoStruct for HashMap.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,8 @@
 // TODO: Remove this
 #![allow(dead_code)]
 
-use std::collections::BTreeMap;
+use std::collections::{ BTreeMap, HashMap };
+use std::hash::Hash;
 
 
 #[derive(Eq, Ord, PartialOrd, PartialEq, Debug, Clone, Copy)]
@@ -19,6 +20,38 @@ trait MemoStruct<K,V: Clone>: IntoIterator<Item=(K,V)>
     fn get(&self, k: &K) -> Option<V>;
     fn get_mut(&mut self, k: &K) -> Option<&mut V>;
     fn iter<'a>(&'a self) -> Box<dyn 'a + Iterator<Item=(&'a K, &'a V)>>;
+}
+
+impl<K, V: Clone> MemoStruct<K,V> for HashMap<K,V>
+where
+    K: Hash + Eq,
+{
+    fn new() -> Self {
+        HashMap::new()
+    }
+    fn insert(&mut self, k: K, v: V) -> Result<(), V> {
+        use std::collections::hash_map::Entry::*;
+        match HashMap::entry(self, k) {
+            Vacant(ve) => {
+                ve.insert(v);
+                Ok(())
+            },
+            Occupied(mut oe) => {
+                let oldv = oe.insert(v);
+                Err(oldv)
+            },
+        }
+    }
+    fn get(&self, k: &K) -> Option<V> {
+        self.get(k).map(|vref| vref.clone())
+    }
+    fn get_mut(&mut self, k: &K) -> Option<&mut V> {
+        HashMap::get_mut(self, k)
+    }
+    fn iter<'a>(&'a self) -> Box<dyn 'a + Iterator<Item=(&'a K, &'a V)>>
+    {
+        Box::new(HashMap::iter(self))
+    }
 }
 
 impl<K,V: Clone> MemoStruct<K,V> for BTreeMap<K,V>
